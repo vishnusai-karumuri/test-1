a0d2c4b9e1da074f4e2050f757bc304dd6872fb5
vamsisai <vamsisai.karumuri@gmail.com>
2024-09-12 23:09:23 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+export default function App ()  {
+
+  return (
+    <div className='w-screen h-screen flex flex-col items-center justify-center'>
+      App
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        'spin': {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      animation: {
+        'spin-slow': 'spin 1s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+}
+
